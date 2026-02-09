afa70e762a7dd15c76c91e9ad049ff2b73071b3b
oscarhernandeziv <oscarhernandeziv@gmail.com>
2026-02-08 22:10:22 -0800

    chore(release): 0.3.5

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -3,7 +3,7 @@
  */
 
 /** SDK version - must match package.json */
-export const VERSION = "0.3.4";
+export const VERSION = "0.3.5";
 
 /**
  * Default patterns to ignore when scanning repositories.
